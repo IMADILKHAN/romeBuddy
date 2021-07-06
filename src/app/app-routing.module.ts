2b0e505511845c6ec0a95d1d0fc4eb52fb8f66da
ADIL KHAN <blueaadil@gmail.com>
2021-07-06 18:53:22 +0530

    Added auth page, reconfigured the routing and changed the themes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
+    path: 'auth',
+    redirectTo: '',
     pathMatch: 'full'
   },
+
 ];
 
 @NgModule({
