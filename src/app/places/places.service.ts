1bc8c62a91d8b0f44fca094239c556ff73d3af76
ADIL KHAN <blueaadil@gmail.com>
2021-07-08 14:57:23 +0530

    Added Edit Page For Booking's added routings to edit the offerings and nav button on the page

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -31,5 +31,8 @@ export class PlacesService {
   get places(){
     return [...this._places]
   }
+  getPlaces(id:string){
+    return {...this._places.find(p=>p.id==id)}
+  }
   constructor() { }
 }
