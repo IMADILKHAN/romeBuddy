9ab6d212d2cd2f1ee7b570e4a7147838760f0b40
ADIL KHAN <blueaadil@gmail.com>
2021-07-13 18:13:05 +0530

    Added auth logic and stored input data

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from './auth.service';
 import {Router} from '@angular/router';
 import {LoadingController} from '@ionic/angular';
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
@@ -14,6 +15,13 @@ export class AuthPage implements OnInit {
   ngOnInit() {
   }
   onLogin(){
+  }
+  onLogout(){
+    this.authService.Logout();
+  }
+
+  onSubmit(form:NgForm){
+    console.log(form.value)
     this.isLoading=true;
     this.authService.Login();
     this.loadingCtrl.create({keyboardClose:true,message:'Logging in '})
@@ -25,11 +33,6 @@ export class AuthPage implements OnInit {
         this.router.navigateByUrl('/places/tabs/discover')
       },1000)
     })
-
-  }
-
-  onLogout(){
-    this.authService.Logout();
   }
 
 }
