7c5d831ffa7b8d3dbde2fdd1bcb4e32254238a01
ADIL KHAN <blueaadil@gmail.com>
2021-07-27 15:12:22 +0530

    Added method to delete bookings from firebase

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -59,14 +59,12 @@ export class BookingService {
   }
 
   cancelBooking(bookingId:string){
-    return this.bookings.pipe(
-      take(1),
-      delay(500),
-      tap(bookings=>{
-        this._bookings.next(bookings.filter(b=> b.id!==bookingId)
-      )
-      })
-    )
+    return this.http.delete(`https://romebuddy-default-rtdb.firebaseio.com/bookings/${bookingId}.json`
+    ).pipe(switchMap(()=>{
+        return this.bookings
+    }),take(1),tap(bookings=>{
+      this._bookings.next(bookings.filter(b=> b.id!==bookingId))
+    }))
   }
 
   fetchBooking(){
